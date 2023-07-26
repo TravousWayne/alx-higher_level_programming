9af74c312aed68f4de03c42552d66b46b215ac6f
TravousWayne <kenlavivsparks1010@gmail.com>
2023-07-26 01:23:57 +0000

    All tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
+const url = process.argv[2];
 
-const url = process.argv[2] || '';
-const storage = {};
-let tmp = '';
-
-request(url, (err, res, body) => {
+request(url, function (err, response, body) {
   if (err) {
-    return console.log(err);
-  }
-
-  const parsedBody = JSON.parse(body);
-
-  for (let i = 0; i < parsedBody.length; i++) {
-    tmp = parsedBody[i].userId;
-    if (parsedBody[i].completed) {
-      storage[tmp] = (storage[tmp] || 0) + 1;
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
     }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-
-  console.log(storage);
 });
